0566af544ae5a683cdc95d4a7a598202e37252f1
Abdelhay Bouramdane <abouramd@student.1337.ma>
2025-02-11 15:19:31 +0100

    @abouramd: some changes

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,137 @@
+return {
+
+  {
+"williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+"clangd",
+"css-lsp",
+"docker-compose-language-service",
+"dockerfile-language-server",
+"hadolint",
+"html-lsp",
+"java-language-server",
+"jdtls",
+"json-lsp",
+"lua-language-server",
+"markdown-toc",
+"markdownlint-cli2",
+"marksman",
+"prisma-language-server",
+"pyright",
+"ruff",
+"shfmt",
+"stylua",
+"tailwindcss-language-server",
+"taplo",
+"vtsls",
+"yaml-language-server",
+      },
+    },
+  },
+
+  {
+"nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+"bash",
+"c",
+"cpp",
+"css",
+"diff",
+"dockerfile",
+"git_config",
+"git_rebase",
+"gitattributes",
+"gitcommit",
+"gitignore",
+"html",
+"java",
+"javascript",
+"jsdoc",
+"json",
+"json5",
+"jsonc",
+"lua",
+"luadoc",
+"luap",
+"markdown",
+"markdown_inline",
+"ninja",
+"printf",
+"prisma",
+"python",
+"query",
+"regex",
+"rst",
+"toml",
+"tsx",
+"typescript",
+"vim",
+"vimdoc",
+"xml",
+"yaml",
+      },
+    },
+  },
+}
+
+-- css-lsp
+-- docker-compose-language-service
+-- dockerfile-language-server
+-- hadolint
+-- html-lsp
+-- java-language-server
+-- jdtls
+-- json-lsp
+-- lua-language-server
+-- markdown-toc
+-- markdownlint-cli2
+-- marksman
+-- prisma-language-server
+-- pyright
+-- ruff
+-- shfmt
+-- stylua
+-- tailwindcss-language-server
+-- taplo
+-- vtsls
+-- yaml-language-server
+
+-- "bash"
+-- "c"
+-- "cpp"
+-- "css"
+-- "diff"
+-- "dockerfile"
+-- "git_config"
+-- "git_rebase"
+-- "gitattributes"
+-- "gitcommit"
+-- "gitignore"
+-- "html"
+-- "java"
+-- "javascript"
+-- "jsdoc"
+-- "json"
+-- "json5"
+-- "jsonc"
+-- "lua"
+-- "luadoc"
+-- "luap"
+-- "markdown"
+-- "markdown_inline"
+-- "ninja"
+-- "printf"
+-- "prisma"
+-- "python"
+-- "query"
+-- "regex"
+-- "rst"
+-- "toml"
+-- "tsx"
+-- "typescript"
+-- "vim"
+-- "vimdoc"
+-- "xml"
+-- "yaml"
